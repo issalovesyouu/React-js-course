7318d83038d8b17656b2b6c9b07d37e5e90e74df
Isabella Kristiansen <iskr@fotoware.com>
2019-01-02 12:41:40 +0100

    React JS course

--- src/App.js
+++ src/App.js
@@ -1,27 +1,138 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Radium, { StyleRoot } from "radium";
+import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { id: "sdsads1", name: "Max", age: 28 },
+      { id: "ewr11", name: "Manu", age: 29 },
+      { id: "dsre111", name: "Isabella", age: 23 }
+    ],
+    otherState: "Some other value",
+    showPersons: false
+  };
+
+  switchNameHandler = newName => {
+    //  console.log("was clicked");
+    // DONT DO THIS: this.state.persons[0].name = "Maximilian";
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Isabella", age: 27 }
+      ]
+    });
+  };
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({
+      persons: persons
+      // [
+      //   { name: "Max", age: 28 },
+      //   { name: event.target.value, age: 29 },
+      //   { name: "Isabella", age: 27 }
+      // ]
+    });
+  };
+
+  deletePersonHandler = personIndex => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
+  togglePeronsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
   render() {
+    const style = {
+      backgroundColor: "green",
+      color: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={event => this.nameChangedHandler(event, person.id)}
+              />
+            );
+          })}
+          {/* <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, "Max!")}
+            changed={this.nameChangedHandler}
+          >
+            My Hobbies: Racing
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          /> */}
+        </div>
+      );
+      style.backgroundColor = "red";
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push("red"); // Classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push("bold"); // classes = ['red', 'bold']
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi, I am a React App</h1>
+        <p className={classes.join(" ")}>This is really working!</p>
+        <button style={style} onClick={this.togglePeronsHandler}>
+          Toogle persons
+        </button>
+        {persons}
       </div>
     );
+    // return React.createElement(
+    //   "div",
+    //   { className: "App" },
+    //   null,
+    //   React.createElement("h1", null, "Does this work now?")
+    // );
   }
 }
 
